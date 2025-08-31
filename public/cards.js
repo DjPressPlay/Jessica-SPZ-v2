978833598671ebcf0b62fad2523fbae236b896eb
Artworqq <keysub.kevin@gmail.com>
2025-08-31 00:54:54 -0400

    Create cards.js

---
+++ public/cards.js
@@ -0,0 +1,13 @@
+// cards.js
+
+window.CardLibrary = {
+  sampleCard: {
+    id: "card001",
+    name: "Zetsu Flame",
+    atk: 2500,
+    def: 1800,
+    img: "https://placehold.co/80x120/ff0066/fff?text=Zetsu",
+    type: "Fire",
+    rarity: "SR"
+  }
+};
