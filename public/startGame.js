8fbbca4f13189c4d8559459da4a466d5c61c17e8
Artworqq <keysub.kevin@gmail.com>
2025-08-31 01:26:07 -0400

    Delete public/startGame.js

--- public/startGame.js
+++
@@ -1,8 +0,0 @@
-// start_game.js
-import { Dealer } from "./Dealer.js";
-import { initCardInfoHover } from "./initCardInfoHover.js";
-
-window.addEventListener("DOMContentLoaded", () => {
-  initCardInfoHover();
-  Dealer.start();
-});
