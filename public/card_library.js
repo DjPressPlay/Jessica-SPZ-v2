0f91ed788ca4ccd9bf0ed3f327a074ddb3316256
Artworqq <keysub.kevin@gmail.com>
2025-08-31 01:06:30 -0400

    Update and rename cards.js to card_library.js

---
+++ public/card_library.js
@@ -0,0 +1,25 @@
+// card_library.js
+export const CardLibrary = {
+  sampleCard: {
+    id: "000057",
+    name: "Shorties Bootleg Broadcast | Adult Swim Bumps | #1",
+    icon: "🌍📰",
+    about: "YouTube",
+    tribute: "🙇‍♂️🙇‍♂️🙇‍♂️🙇‍♂️🙇‍♂️",
+    effects: [
+      { icons: "🌍📰", emoji: "🌍", text: "By DOOM" },
+      { icons: "🌍📰", emoji: "🌍", text: "Adult Swim Bumps | #1" }
+    ],
+    atk: 4200,
+    def: 3360,
+    level: 8,
+    tributes: 8,
+    rarity: "UR",
+    card_sets: ["YouTube", "2025 YouTube"],
+    card_images: [
+      {
+        image_url: "https://i.ytimg.com/vi/KNCJTC_thLA/hqdefault.jpg"
+      }
+    ]
+  }
+};
